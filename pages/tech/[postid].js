3147e51523dcdc5e0e92bdeb64224d832cceb73a
Gerardo Martinez <gmarr@192.168.1.6>
2021-12-18 09:31:24 -0600

    Adding Markdonw

--- pages/tech/[postid].js
+++ pages/tech/[postid].js
@@ -1,10 +1,23 @@
-export default function Post({results}) {
-    console.log(results)
+import markdownToHtml from "../../lib/markdowntohtml"
+import markdownStyles from './markdown-styles.module.css'
+
+export default function Post({results,contenido}) {
+    const blog = results[0]
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="mx-6">
-                <p>{results[0]["content"]}</p>
+        <div className="flex-col items-center justify-center min-h-screen mt-9">
+            {/* Title */}
+            <div className="flex-col items-center justify-start ml-6 md:ml-36 space-y-2">
+                <h1 className="text-5xl font-bold">{blog["title"]}</h1>
             </div>
+            <div className="flex-col items-center justify-start ml-6 md:ml-36 mt-2">
+                <p className="text-sm font-extralight"><span className="font-normal">Fecha: </span> {blog["published_at"].slice(0,10)}</p>
+                <p className="text-sm font-extralight"><span className="font-normal">Autor: </span> {blog["user"]["username"]}</p>
+            </div>
+            <div className="mx-8 md:mx-36 my-6">
+                <div className={markdownStyles["markdown-body"]} dangerouslySetInnerHTML={{ __html: contenido }}>
+                </div>
+            </div>
+
         </div>
     )
 }
@@ -20,10 +33,13 @@ export async function getStaticProps({params}){
             article_post.push(element)
         }
     });
-
+    
+    const content = await markdownToHtml(article_post[0].content || "")
+    
     return{
         props:{
-            results:article_post
+            results:article_post,
+            contenido:content
         }
     }
 }
